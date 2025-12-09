b157eddb1da915f4768abe4f58cba3cabe87a925
6704062610089 <s6704062610089@email.kmutnb.ac.th>
2025-12-09 13:50:19 +0700

    input > clock > output

---
+++ clock.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int num1,num2,num3;
+    scanf("%d:%d:%d",&num1,&num2,&num3);
+    printf("hour:%d\n",num1);
+    printf("minute:%d\n",num2);
+    printf("second:%d\n",num3);
+    return 0;
+}
